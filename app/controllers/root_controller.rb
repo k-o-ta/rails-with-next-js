42900a2f3cbf75cd0b977e9d728bd87853f04b81
Koji Ota <afterjnih@gmail.com>
2022-03-14 02:13:09 +0900

    test

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -1,6 +1,6 @@
 class RootController < ApplicationController
   def index
-    render plain: "OK"
-    # render file: "#{Rails.root}/public/out/index.html", layout: false
+    # render plain: "OK"
+    render file: "#{Rails.root}/public/out/index.html", layout: false
   end
 end
